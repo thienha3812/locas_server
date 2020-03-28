9c2892bf22f3e43f95dbd104821b6a761d5186ab
Your Name <you@example.com>
2020-03-28 23:26:03 +0700

    123

--- app.js
+++ app.js
@@ -13,12 +13,15 @@ require('./database/connect')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('etag', false); 
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', indexRouter);
 app.use('api/users', usersRouter);
