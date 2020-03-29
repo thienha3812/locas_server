a9a0f8b3c3bd712176e736e2ebb567b4dfe24917
Your Name <you@example.com>
2020-03-29 22:08:29 +0700

    123

--- routes/places.js
+++ routes/places.js
@@ -37,11 +37,23 @@ router.post('/getplacesfromward',async(req,res,next)=>{
         return res.sendStatus(500)
     }
 })
-router.post('/getplacefrominfor', async (req,res,next)=>{
+router.post('/getallfromdetail', async (req,res,next)=>{
     try  {
-
-    }catch(err){
         const { address_components} = req.body
+        const xa_phuong = '%'  + address_components[0].long_name  + '%'
+        const quan_huyen ='%' +  address_components[1].long_name + '%'
+        const tinh_thanh = '%' +  address_components[2].long_name   +   '%'
+        const infor =  await sequelize.query("SELECT * FROM xa_phuong LEFT JOIN quan_huyen ON quan_huyen.ma_qh = xa_phuong.ma_qh LEFT JOIN tinh_thanh ON tinh_thanh.ma_tinh = quan_huyen.ma_tinh WHERE xa_phuong.ten_xap LIKE :xa_phuong AND quan_huyen.ten_qh LIKE :quan_huyen AND tinh_thanh.ten_tinh LIKE :tinh_thanh",{            
+            replacements:{
+                xa_phuong,
+                quan_huyen,
+                tinh_thanh
+            },
+            type : Sequelize.QueryTypes.SELECT
+        })
+        return res.status(200).json({infor,message:"Lấy thông tin thành công",code : 1})        
+    }catch(err){
+        return res.sendStatus(500)
     }
 })
 module.exports = router
