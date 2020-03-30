c47bb3939aa66e6d61536d080bcb8e534e03e362
Your Name <you@example.com>
2020-03-30 23:01:41 +0700

    123

--- routes/places.js
+++ routes/places.js
@@ -22,10 +22,10 @@ router.get('/all', async (req, res, next) => {
 })
 router.post('/getplacesfromward', async (req, res, next) => {
     try {
-        const { ward_id } = req.body
-        const places = await sequelize.query("SELECT dia_diem.*,AVG(danh_gia.rating ) as rating FROM dia_diem LEFT JOIN danh_gia ON danh_gia.ma_dd = dia_diem.ma_dd WHERE ma_xap = :ward_id GROUP BY dia_diem.ma_dd ORDER BY rating DESC", {
+        const { ward } = req.body
+        const places = await sequelize.query("SELECT dia_diem.*,AVG(danh_gia.rating ) as rating FROM dia_diem LEFT JOIN danh_gia ON danh_gia.ma_dd = dia_diem.ma_dd WHERE ma_xap = :ward GROUP BY dia_diem.ma_dd ORDER BY rating DESC", {
             replacements: {
-                ward_id: ward_id
+                ward: ward
             },
             type: Sequelize.QueryTypes.SELECT
         })
