33bb5a2ef19e177ee61bd965c19f3e0c3de7ebb8
Your Name <you@example.com>
2020-03-31 00:43:26 +0700

    123

--- routes/places.js
+++ routes/places.js
@@ -37,20 +37,20 @@ router.post('/getplacesfromward', async (req, res, next) => {
         return res.sendStatus(500)
     }
 })
-router.post('/getallfromwardid', async( req,res,next)=>{
-        try {
-            const  {ward_id} = req.body // Mã xã phường
-            const places = await sequelize.query('SELECT * FROM xa_phuong LEFT JOIN quan_huyen ON quan_huyen.ma_qh = xa_phuong.ma_qh LEFT JOIN tinh_thanh ON tinh_thanh.ma_tinh = quan_huyen.ma_tinh WHERE xa_phuong.ma_xap = :ward_id',{
-                replacements : {
-                    ward_id : ward_id,
-                },
-                type : Sequelize.QueryTypes.SELECT
-            })
-            return res.status(200).json({places,message : "Lấy danh sách thành công",code:1})
-            
-        }catch(err){
-            return res.sendStatus(500)
-        }
+router.post('/getallfromwardid', async (req, res, next) => {
+    try {
+        const { ward_id } = req.body // Mã xã phường
+        const places = await sequelize.query('SELECT * FROM xa_phuong LEFT JOIN quan_huyen ON quan_huyen.ma_qh = xa_phuong.ma_qh LEFT JOIN tinh_thanh ON tinh_thanh.ma_tinh = quan_huyen.ma_tinh WHERE xa_phuong.ma_xap = :ward_id', {
+            replacements: {
+                ward_id: ward_id,
+            },
+            type: Sequelize.QueryTypes.SELECT
+        })
+        return res.status(200).json({ places, message: "Lấy danh sách thành công", code: 1 })
+
+    } catch (err) {
+        return res.sendStatus(500)
+    }
 })
 router.post('/getallfromdetail', async (req, res, next) => {
     try {
@@ -75,29 +75,29 @@ router.post('/getplacesinrange', async (req, res, next) => {
     try {
         const { distance, coordinate } = req.body
         var places = await sequelize.query('SELECT dia_diem.*,COALESCE(AVG(danh_gia.rating),0) as rating, COALESCE(count(danh_gia.rating),0)  as count FROM dia_diem LEFT JOIN danh_gia ON dia_diem.ma_dd = danh_gia.ma_dd LEFT JOIN nd_yeu_thich_dia_diem on nd_yeu_thich_dia_diem.ma_dd = dia_diem.ma_dd GROUP BY dia_diem.ma_dd', {
-            type: Sequelize.QueryTypes.SELECT,            
+            type: Sequelize.QueryTypes.SELECT,
         })
         let results = []
-        places.forEach(x=>{
+        places.forEach(x => {
             const start = {
-                latitude : coordinate.lat,
-                longitude : coordinate.lng
+                latitude: coordinate.lat,
+                longitude: coordinate.lng
             }
             const end = {
-                latitude : x.toa_do.lat,
-                longitude : x.toa_do.lng
+                latitude: x.toa_do.lat,
+                longitude: x.toa_do.lng
             }
-            const d = haversine(start, end, {unit: 'meter'})            
-            if(d<distance){
+            const d = haversine(start, end, { unit: 'meter' })
+            if (d < distance) {
                 results.push(x)
             }
         })
-        return res.json({message:"Lấy danh sách thành công",results})
-    } catch (err) {        
+        return res.json({ message: "Lấy danh sách thành công", results })
+    } catch (err) {
         return res.sendStatus(500)
     }
 })
-router.post('/insertplacefromuser',upload.any(),async(req,res,next)=>{
+router.post('/insertplacefromuser', upload.any(), async (req, res, next) => {
     try {
         const token = req.headers["authorization"]
         const decoded = jwt.verify(token, 'secret', (err, decoded) => {
@@ -105,45 +105,49 @@ router.post('/insertplacefromuser',upload.any(),async(req,res,next)=>{
             return decoded
         })
         const user_id = decoded.id
-        const image = req.files.filter(x => x.fieldname === 'hinh_anh')[0]        
-        const logo = req.files.filter(x => x.fieldname === 'logo')[0]                
-        const {ten_dd,mo_ta,toa_do,gio_mo_cua,gio_dong_cua,ma_dm,ma_xap,dia_chi,luoc_su} = req.body
+        const image = req.files.filter(x => x.fieldname === 'hinh_anh')[0]
+        const logo = req.files.filter(x => x.fieldname === 'logo')[0] != null ? eq.files.filter(x => x.fieldname === 'logo')[0] : null
+        const { ten_dd, mo_ta, toa_do, gio_mo_cua, gio_dong_cua, ma_dm, ma_xap, dia_chi, luoc_su } = req.body
         const imagePath = path.join('./uploads/').concat(uuid.v4() + path.extname(image.originalname))
-        const logoPath = path.join('./uploads/').concat(uuid.v4() + path.extname(logo.originalname))                 
-        await sequelize.query("INSERT INTO dia_diem(ten_dd, mo_ta, toa_do, logo, gio_mo_cua, gio_dong_cua, luoc_su_hinh_thanh,nguoi_tao,ma_xap,hinh_anh,ma_dm,dia_chi) VALUES(:ten_dd,:mo_ta,:toa_do,:logo,:gio_mo_cua,:gio_dong_cua,:luoc_su,:nguoi_tao,:ma_xap,:hinh_anh,:ma_dm,:dia_chi) ",{
-            replacements : {
-                ten_dd : ten_dd,
-                mo_ta : mo_ta,
-                toa_do : toa_do,
-                logo : 'http://149.28.145.107:8000/' + logoPath,
-                gio_mo_cua : gio_mo_cua,
-                gio_dong_cua : gio_dong_cua,
-                nguoi_tao : user_id,
-                ma_xap : ma_xap,
-                hinh_anh : 'http://149.28.145.107:8000/'+ imagePath,
-                ma_dm : ma_dm,
-                dia_chi : dia_chi,
-                luoc_su : luoc_su
+        let logoPath
+        if (logo != null) {
+            logoPath = path.join('./uploads/').concat(uuid.v4() + path.extname(logo.originalname))
+        }
+        await sequelize.query("INSERT INTO dia_diem(ten_dd, mo_ta, toa_do, logo, gio_mo_cua, gio_dong_cua, luoc_su_hinh_thanh,nguoi_tao,ma_xap,hinh_anh,ma_dm,dia_chi) VALUES(:ten_dd,:mo_ta,:toa_do,:logo,:gio_mo_cua,:gio_dong_cua,:luoc_su,:nguoi_tao,:ma_xap,:hinh_anh,:ma_dm,:dia_chi) ", {
+            replacements: {
+                ten_dd: ten_dd,
+                mo_ta: mo_ta,
+                toa_do: toa_do,
+                logo: logo != null ? 'http://149.28.145.107:8000/' + logoPath : '',
+                gio_mo_cua: gio_mo_cua,
+                gio_dong_cua: gio_dong_cua,
+                nguoi_tao: user_id,
+                ma_xap: ma_xap,
+                hinh_anh: 'http://149.28.145.107:8000/' + imagePath,
+                ma_dm: ma_dm,
+                dia_chi: dia_chi,
+                luoc_su: luoc_su
             },
-            type : Sequelize.QueryTypes.INSERT
-        })        
-        const p1 = Promise.resolve(fs.writeFileSync(imagePath,image.buffer))
-        const p2 = Promise.resolve(fs.writeFileSync(logoPath,logo.buffer))
-        Promise.all([p1,p2]).then(()=>{
-            return res.json({message : "Thêm thành công",code : 1})
-        }).catch(err=>{
+            type: Sequelize.QueryTypes.INSERT
+        })
+        const p1 = Promise.resolve(fs.writeFileSync(imagePath, image.buffer))
+        const p2 = logo != null ? Promise.resolve(fs.writeFileSync(logoPath, logo.buffer)) : null
+        Promise.all([p1, p2]).then(() => {
+            return res.json({ message: "Thêm thành công", code: 1 })
+        }).catch(err => {
             throw new Error()
         })
-        
-    }catch(err){          
+
+    } catch (err) {
+        console.log(err)
         return res.sendStatus(500)
     }
 })
-router.post('/addfavoriteplacefromuser',async(req,res,next)=>{
-    try { 
+router.post('/addfavoriteplacefromuser', async (req, res, next) => {
+    try {
 
-    }catch(err){
-        
+    } catch (err) {
+
     }
 })
 module.exports = router
